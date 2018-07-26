11668269608f5166af6b18f110d19c9d379de89a
bob <bob.crutchley.qa@gmail.com>
2018-07-26 19:09:43 +0100

    removed redundant DAL classes

--- src/main/java/org/bob/spring/elasticsearch/example/dal/UserDAL.java
+++
@@ -1,9 +0,0 @@
-package org.bob.spring.elasticsearch.example.dal;
-
-import org.bob.spring.elasticsearch.example.model.User;
-
-import java.util.List;
-
-public interface UserDAL {
-    List<User> getAll();
-}
